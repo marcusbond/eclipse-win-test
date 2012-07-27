25b6dc3a966a4677163151deccf9b95c75a8502b
Marcus <Marcus@MBelitebook>
2012-07-27 13:02:55 +0100

    Adding to git

---
+++ github-works/src/com/marcusbond/example/github/GitHubWorks.java
@@ -0,0 +1,15 @@
+package com.marcusbond.example.github;
+
+/**
+ * 
+ * @author Marcus Bond
+ *
+ */
+public class GitHubWorks {
+
+	public static void main(String[] args) {
+		System.out.println("GitHub works !");
+
+	}
+
+}
